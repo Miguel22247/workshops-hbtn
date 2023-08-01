456211af706ab8a5aac46f5523b8b53c10fa3f02
Miguel Pacheco <minipachru@gmail.com>
2023-08-01 12:58:28 -0300

    add basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Header, Title } from "./app-styles";
+import * as S from "./app-styles";
+import { BasicComponent } from "./components/basics";
 
 function App() {
   return (
@@ -7,6 +9,10 @@ function App() {
       <Header>
         <Title>Holberton x Blast workshop of Styled Components</Title>
       </Header>
+      <S.Wrapper>
+        <S.Subtitle>Basics of Styled Components</S.Subtitle>
+        <BasicComponent />
+      </S.Wrapper>
       {/* // TODO: Show how to import components from a JS file using both methods */}
     </>
   );
