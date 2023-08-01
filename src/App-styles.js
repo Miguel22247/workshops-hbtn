7e8f5427013f589d87714f48156e750b0eb85925
Miguel Pacheco <minipachru@gmail.com>
2023-08-01 10:06:02 -0300

    add files for the workshop

---
+++ src/App-styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background-color: #282c34;
+  min-height: 10vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(1px + 2vmin);
+  color: white;
+`;
+
+export const Wrapper = styled.section``;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    {/* // TODO: Show how to import components from a JS file using both methods */}
+    </>
+  );
+}
+
+export default App;
