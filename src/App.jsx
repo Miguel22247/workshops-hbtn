5c35cbca05f5caf34b311db3d502bc64f12c3552
Miguel Pacheco <minipachru@gmail.com>
2023-08-01 16:06:38 -0300

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Header, Title } from "./app-styles";
 import * as S from "./app-styles";
 import { BasicComponent } from "./components/basics";
+import { ExtendingStyling } from "./components/extending-styles";
+import { StylingAnything } from "./components/styling-any-component";
+import { StylingWithPropsAndStates } from "./components/with-props";
 
 function App() {
   return (
@@ -12,6 +15,21 @@ function App() {
       <S.Wrapper>
         <S.Subtitle>Basics of Styled Components</S.Subtitle>
         <BasicComponent />
+      </S.Wrapper>
+      <S.Wrapper>
+        <S.Subtitle>Extending Styled Components</S.Subtitle>
+        <ExtendingStyling />
+      </S.Wrapper>
+      <S.Wrapper>
+        <S.Subtitle>Shared Styled Components</S.Subtitle>
+      </S.Wrapper>
+      <S.Wrapper>
+        <S.Subtitle>Styling Any Components</S.Subtitle>
+        <StylingAnything />
+      </S.Wrapper>
+      <S.Wrapper>
+        <S.Subtitle>Styling Components with Props or States</S.Subtitle>
+        <StylingWithPropsAndStates />
       </S.Wrapper>
       {/* // TODO: Show how to import components from a JS file using both methods */}
     </>
