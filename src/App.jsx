898704f9c9a2f3c3df07a5e196cfc0d123054857
Miguel Pacheco <minipachru@gmail.com>
2024-02-09 14:56:51 -0300

    adds required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Gallery from "./components/Gallery";
+import SharesDataBetweenComponents from "./components/SharesData";
+import CountsSeparately from "./components/CountsSeparately";
 
 function App() {
   return (
-    <div>
-      <Gallery />
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+      <div>
+        <h2>Counters that updates separately</h2>
+        <CountsSeparately />
+      </div>
+      <div>
+        <h2>Counters that update together</h2>
+        <SharesDataBetweenComponents />
+      </div>
+      <div>
+        <h2>Final example</h2>
+        <Gallery />
+      </div>
     </div>
   );
 }
