13213340dd295c26781b1258e797fff97e19ac83
Miguel Pacheco <minipachru@gmail.com>
2024-02-14 09:56:35 -0300

    add example of shared state

--- src/components/SharesData.jsx
+++ src/components/SharesData.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
+import MyButtonShared from "./Buttons/MyButtonShared";
 
 export default function SharesDataBetweenComponents() {
-  
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <MyButtonShared count={count} handleClick={handleClick} />
+      <MyButtonShared count={count} handleClick={handleClick} />
+    </>
+  );
 }
